b525a32bbae0da84367b823f2d9b33ba31d8cf38
basti1dr <basti1.dr@gmail.com>
2013-11-14 22:45:58 +0100

    Delete unuse import

--- src/com/lifechievements/MainActivity.java
+++ src/com/lifechievements/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
