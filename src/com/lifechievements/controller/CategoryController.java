8e07d9bb19143fd70e64577c42a5c826565379d3
yen0x <gege.dert@gmail.com>
2013-11-21 00:15:17 +0100

    Added the findAll method for categories

--- src/com/lifechievements/controller/CategoryController.java
+++ src/com/lifechievements/controller/CategoryController.java
@@ -1,16 +1,22 @@
 package com.lifechievements.controller;
 
+import java.util.List;
+
+import android.content.Context;
+
+import com.lifechievements.dao.CategoryDAO;
+import com.lifechievements.model.Category;
+
 public class CategoryController {
 
-	/*
-	 * Default constructor
-	 */
-	public CategoryController(){
-		//TODO : initialization
+	private CategoryDAO dao;
+
+	public CategoryController(Context context) {
+		dao = new CategoryDAO(context);
 	}
-	
-	public void findAllCategory(){
-		
+
+	public List<Category> findAll() {
+		return dao.findAll();
 	}
-	
+
 }
--- src/com/lifechievements/dao/CategoryDAO.java
+++ src/com/lifechievements/dao/CategoryDAO.java
@@ -1,7 +1,21 @@
 package com.lifechievements.dao;
 
+import java.util.List;
+
+import com.lifechievements.model.Category;
+
+import android.content.Context;
+
 public class CategoryDAO {
-	
+
 	private JsonDAO jdao;
+
+	public CategoryDAO(Context context) {
+		jdao = new JsonDAO(context);
+	}
+
+	public List<Category> findAll() {
+		return jdao.getJsonDb();
+	}
 
 }
