d344104634a10d1dce0cc0a695be4731d2056a92
basti1dr <basti1.dr@gmail.com>
2014-08-09 14:54:15 +0200

    Add package (of course i move the file), add import, commnet load from file because not working --> Update TODO

--- src/com/lifechievements/SettingsActivity.java
+++ src/com/lifechievements/SettingsActivity.java
@@ -1,9 +1,14 @@
+package com.lifechievements;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
 public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Load the preferences from XML
-        addPreferencesFromResource(R.xml.preferences);
+        //addPreferencesFromResource(R.xml.preferences);
     }
 }
